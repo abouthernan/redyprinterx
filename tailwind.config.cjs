4a9a30f832895401c13d5d9e7037da74656aab49
Cristhian Melo <rxtsel@hotmail.com>
2023-06-02 10:58:16 -0500

    chore: add custom colors and fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif']
+      },
+      colors: {
+        marino: '#2F455C',
+        cielo: '#1DCDFE',
+        turquesa: '#34F5C6',
+        aguamarina: '#21D0B3',
+        blanco: '#ebf4fc'
+      }
+    }
+  },
+  plugins: []
 }
