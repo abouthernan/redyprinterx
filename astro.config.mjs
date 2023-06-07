734917897540f8978280ef8a27f44a964583d3be
Cristhian Melo <rxtsel@hotmail.com>
2023-06-07 09:45:09 -0500

    feat: add github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import preact from '@astrojs/preact'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), preact()]
+  integrations: [tailwind(), preact()],
+  site: 'https://abouthernan.github.io',
+  base: '/redyprinterx'
 })
