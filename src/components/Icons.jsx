d1a571210940427304711f89af8b8beb83776139
Cristhian Melo <rxtsel@hotmail.com>
2023-06-09 17:51:57 -0500

    fix: add color to burger

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -2,7 +2,7 @@ export function Menu() {
   return (
     <svg
       stroke="currentColor"
-      fill="currentColor"
+      fill="#1d1d1d"
       stroke-width="0"
       viewBox="0 0 24 24"
       xmlns="http://www.w3.org/2000/svg"
@@ -16,7 +16,7 @@ export function Close() {
   return (
     <svg
       stroke="currentColor"
-      fill="currentColor"
+      fill="#1d1d1d"
       stroke-width="0"
       viewBox="0 0 24 24"
       xmlns="http://www.w3.org/2000/svg"
