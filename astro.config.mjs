5ce31f0a9af9d97d10a56b5e8c7cebc2b40f3783
Cristhian Melo <rxtsel@hotmail.com>
2023-06-09 17:34:12 -0500

    chore: add custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import preact from '@astrojs/preact'
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), preact()],
-  site: 'https://abouthernan.github.io',
-  base: '/redyprinterx'
+  site: 'https://rypx.vehement.co',
+  base: '/'
 })
