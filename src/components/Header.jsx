4abd92204664e720fa09e10a5d37e5202a8f20a7
Cristhian Melo <rxtsel@hotmail.com>
2023-10-24 15:50:12 -0500

    chore: change logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'preact/hooks'
 import { Menu, Close } from './Icons'
-import logoImg from '/logo.svg'
+import logoImg from '/logo-large.png'
 
 export function Header() {
   const [menuIsOpen, setMenuIsOpen] = useState(false)
@@ -44,7 +44,7 @@ export function Header() {
     const megaMenuDropdown = document.getElementById('mega-menu-dropdown')
 
     // si doy click fuera del menu se cierra
-    window.addEventListener('click', (e) => {
+    window.addEventListener('click', e => {
       if (!e.target.matches('#mega-menu-dropdown-button')) {
         if (!e.target.matches('#mega-menu-dropdown')) {
           megaMenuDropdown.classList.add('hidden')
@@ -58,8 +58,7 @@ export function Header() {
 
     megaMenuButton.addEventListener('mouseenter', () => {
       megaMenuDropdown.classList.remove('hidden')
-    }
-    )
+    })
     megaMenuDropdown.addEventListener('mouseleave', () => {
       megaMenuDropdown.classList.add('hidden')
     })
@@ -67,25 +66,28 @@ export function Header() {
 
   useEffect(() => {
     megaMenu()
-  }
-  )
+  })
 
   return (
     <header
       className={`w-full fixed  bg-white z-20 top-0 transition-all duration-500 ${showHeader} animate-in fade-in duration-1000`}
     >
       <div class="max-w-7xl flex h-[72px] justify-between p-2 md:py-2 md:px-0 items-center mx-auto">
-        <a href="/" className="h-14 w-14 transition-transform duration-300 flex-grow-0">
+        <a
+          href="/"
+          className="h-28 lg:h-36 transition-transform duration-300 flex-grow-0"
+        >
           <img
-            className="aspect-square object-cover object-left w-full h-full"
+            className="aspect-video object-contain w-full h-full"
             src={logoImg}
             alt="Logo"
           />
         </a>
 
         <nav
-          className={`flex bg-blanco rounded-t-3xl md:bg-transparent fixed md:static top-[72px] left-0 right-0 h-screen md:h-auto transition-all translate-x-full md:translate-x-0 duration-500 opacity-0 md:opacity-100 ${menuIsOpen ? 'translate-x-0 opacity-100' : ''
-            }`}
+          className={`flex bg-blanco rounded-t-3xl md:bg-transparent fixed md:static top-[72px] left-0 right-0 h-screen md:h-auto transition-all translate-x-full md:translate-x-0 duration-500 opacity-0 md:opacity-100 ${
+            menuIsOpen ? 'translate-x-0 opacity-100' : ''
+          }`}
         >
           <ul className="flex flex-col md:flex-row gap-4 w-full items-center mt-14 md:mt-0">
             <li className="font-medium hover:text-cielo/80 transition-colors duration-300 w-full">
@@ -103,26 +105,58 @@ export function Header() {
             </li>
 
             <li>
-              <button id="mega-menu-dropdown-button" data-dropdown-toggle="mega-menu-dropdown" class="flex items-center justify-between w-full py-2 pl-3 pr-4 font-medium hover:text-cielo/80 transition-colors duration-300">
-                Equipos <svg class="w-2.5 h-2.5 ml-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4" />
+              <button
+                id="mega-menu-dropdown-button"
+                data-dropdown-toggle="mega-menu-dropdown"
+                class="flex items-center justify-between w-full py-2 pl-3 pr-4 font-medium hover:text-cielo/80 transition-colors duration-300"
+              >
+                Equipos{' '}
+                <svg
+                  class="w-2.5 h-2.5 ml-2.5"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 10 6"
+                >
+                  <path
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="m1 1 4 4 4-4"
+                  />
                 </svg>
               </button>
-              <div id="mega-menu-dropdown" class="absolute w-auto min-w-max z-10 hidden bg-white border border-blanco rounded-lg shadow-md  md:grid-cols-3 ">
+              <div
+                id="mega-menu-dropdown"
+                class="absolute w-auto min-w-max z-10 hidden bg-white border border-blanco rounded-lg shadow-md  md:grid-cols-3 "
+              >
                 <div class="p-4">
-                  <ul class="space-y-4" aria-labelledby="mega-menu-dropdown-button">
+                  <ul
+                    class="space-y-4"
+                    aria-labelledby="mega-menu-dropdown-button"
+                  >
                     <li class="w-full">
-                      <a href="/equipos/extraorales" class="text-marino hover:text-cielo/80 transition-colors duration-300 w-full block">
+                      <a
+                        href="/equipos/extraorales"
+                        class="text-marino hover:text-cielo/80 transition-colors duration-300 w-full block"
+                      >
                         Equipos Extraorales
                       </a>
                     </li>
                     <li class="w-full">
-                      <a href="#" class="text-marino hover:text-cielo/80 transition-colors duration-300 w-full block">
+                      <a
+                        href="#"
+                        class="text-marino hover:text-cielo/80 transition-colors duration-300 w-full block"
+                      >
                         Equipos Intraorales
                       </a>
                     </li>
                     <li class="w-full">
-                      <a href="#" class="text-marino hover:text-cielo/80 transition-colors duration-300 w-full block">
+                      <a
+                        href="#"
+                        class="text-marino hover:text-cielo/80 transition-colors duration-300 w-full block"
+                      >
                         Software's
                       </a>
                     </li>
@@ -150,8 +184,9 @@ export function Header() {
         </a>
 
         <button
-          className={`md:hidden w-8 h-8 tex-black flex items-center justify-center border-none outline-none appearance-none ${menuIsOpen ? 'open' : ''
-            }`}
+          className={`md:hidden w-8 h-8 tex-black flex items-center justify-center border-none outline-none appearance-none ${
+            menuIsOpen ? 'open' : ''
+          }`}
           onClick={() => setMenuIsOpen(!menuIsOpen)}
           id="burger"
         >
