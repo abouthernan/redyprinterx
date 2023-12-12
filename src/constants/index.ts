bd3c61a4fb94a30c988ca3bb78e6831ff7108889
Cristhian Melo <rxtsel@hotmail.com>
2023-12-12 13:39:52 -0500

    chore: update proyect

---
+++ src/constants/index.ts
@@ -0,0 +1,8 @@
+export const socialMedia = {
+  facebook: 'https://facebook.com/user',
+  twitter: 'https://twitter.com/user',
+  instagram: 'https://instagram.com/user',
+  whatsapp:
+    'https://api.whatsapp.com/send?phone=573184165736&text=%C2%A1Hola,%20Cristhian!%20%C2%BFC%C3%B3mo%20est%C3%A1s?',
+  linkedin: 'https://linkedin.com/in/user'
+}
